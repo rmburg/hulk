791d9d427ca412ef0574a0facfb7cb5cbe0940ae
Alexander Schmidt <alexander.schmidt@tuhh.de>
2026-09-16 17:47:48 +0000

    Launch motion node

--- crates/hulk_ros_z/src/main.rs
+++ crates/hulk_ros_z/src/main.rs
@@ -161,6 +161,7 @@ async fn spawn_all(ctx: Arc<Context>, log_path: Option<PathBuf>) -> Result<Runni
     join_set.spawn(message_filter::run_boxed(ctx.clone()));
     join_set.spawn(message_handler::run_boxed(ctx.clone()));
     join_set.spawn(microphone_recorder::run_boxed(ctx.clone()));
+    join_set.spawn(motion::run_boxed(ctx.clone()));
     join_set.spawn(motion_inference::run_boxed(ctx.clone()));
     join_set.spawn(motor_commands_collector::run_boxed(ctx.clone()));
     join_set.spawn(obstacle_filter::run_boxed(ctx.clone()));
